e28b9dd1b3a465a48e7730fa8aa62b18b91584f9
Terry Kubalewski <terrykub@yahoo.com>
2023-01-22 18:44:53 -0600

    finally got data from getWeatherInfo() to populate in cards

--- js/main.js
+++ js/main.js
@@ -29,46 +29,17 @@ form.addEventListener('submit', handleFormSubmit);
 
 
 
-// //////////////////////
+// Function to convert lat/lon to city and get city info
+async function getWeatherInfo(cityName){    
+        let response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${apiKey}`)
+        let location = await response.json();
+        let myLat = location[0].lat;
+        let myLon = location[0].lon;
+        let response2 = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${myLat}&lon=${myLon}&appid=${apiKey}`)
+        cityDetails = await response2.json()
+        return cityDetails
+};  
 
-async function getCityInfo(cityName){
-    let coords = []
-    let response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${apiKey}`)
-    let location = await response.json();
-    let myLat = location[0].lat;
-    coords.push(myLat)
-    console.log(myLat);
-    let myLon = location[0].lon;
-    coords.push(myLon)
-    console.log(myLon);
-    return coords
-} 
-
-async function getWeatherInfo(cityName){
-    getCityInfo(cityName)
-    .then(console.log(coords))
-};
-/////////////////////////
-
-// Function to convert lat/lon to city
-// async function getWeatherInfo(cityName){
-
-//     async function getCityInfo(){
-//         let response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${apiKey}`)
-//         let location = await response.json();
-//         let myLat = location[0].lat;
-//         console.log(myLat);
-//         let myLon = location[0].lon;
-//         console.log(myLon);
-//         return location
-//     }  
-
-//     console.log(getCityInfo(cityName))
-//     let response2 = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${cityName.lat}&lon=${cityName.lon}&appid=${apiKey}`)
-//     let cityInfo = await response2.json()
-//     return cityInfo
-
-// };
 
 
 
@@ -91,7 +62,46 @@ function buildWeatherCard(cityInfo){
 
         let cityTitle = document.createElement('h5');
         cityTitle.className = 'card-title';
-        cityTitle.innerHTML = location.name;
+        cityTitle.innerHTML = cityDetails.cityName;
+
+        let cityDescription = document.createElement('p');
+        cityDescription.className = 'card-text';
+        cityDescription.innerHTML = `Today's Weather: ${cityInfo.current.weather[0].description}`;
+
+        let cityCurrent = document.createElement('p');
+        cityCurrent.className = 'card-text';
+        cityCurrent.innerHTML = `Current Temp: ${((cityInfo.current.temp - 273.15)*1.8+32).toFixed(2)}`;
+
+        let cityFeelLike = document.createElement('p');
+        cityFeelLike.className = 'card-text';
+        cityFeelLike.innerHTML = `Feels Like: ${((cityInfo.current.feels_like - 273.15)*1.8+32).toFixed(2)}`;
+
+        let cityHigh = document.createElement('p');
+        cityHigh.className = 'card-text';
+        cityHigh.innerHTML = `Today's High: ${((cityInfo.daily[0].temp.max - 273.15)*1.8+32).toFixed(2)}`;
+
+        let cityLow = document.createElement('p');
+        cityLow.className = 'card-text';
+        cityLow.innerHTML = `Today's Low: ${((cityInfo.daily[0].temp.min - 273.15)*1.8+32).toFixed(2)}`;
+
+        cardBody.append(cityTitle);
+        cardBody.append(cityDescription);
+        cardBody.append(cityCurrent);
+        cardBody.append(cityFeelLike);
+        cardBody.append(cityHigh);
+        cardBody.append(cityLow);
+
+        card.append(cardBody);
+
+        let col = document.createElement('div');
+        col.className = 'col-12 col-md-6 col-lg-3 my-3';
+
+        col.append(card);
+
+        let display = document.getElementById('weatherDisplay');
+        display.append(col);
+
+
+};
 
         
-}
